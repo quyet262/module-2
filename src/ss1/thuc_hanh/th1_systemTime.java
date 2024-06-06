474e5afbeef2f1dae5d0ac78d21215f19f03e9ed
quyet262 <169133354+quyet262@users.noreply.github.com>
2024-06-06 14:11:27 +0700

    l1

---
+++ src/ss1/thuc_hanh/th1_systemTime.java
@@ -0,0 +1,10 @@
+package ss1.thuc_hanh;
+
+import java.util.Date;
+
+public class th1_systemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
